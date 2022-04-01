e6ff1c19ed444a6a9a68773b77c7266ac7a21cc2
satorukishi <satoru_kishi@hotmail.com>
2022-03-31 21:13:25 -0300

    Print, input, variavel, if/else

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
+# Imprime na tela
 print('Hello World')
 print('Meu primeiro código em Python!')
 
+#Declaração de variável. Pode ser de diversos tipo como:
+#str para armazenar textos
+#int para armazenar números inteiros
+#float para armazenar números com pontos flutuante (casas decimais)
+#etc
 numero = 23
 
 print(numero)
@@ -29,17 +35,19 @@ print(mensagem)
 idade = 15
 print(nome, 'tem', idade, 'anos.')
 
-
+# input serve para solicitar ao usuário que digite alguma coisa. O que ele digitar será atribuído a variável "idade"
 idade = input('Informe sua idade:')
 idade = int(idade)
 
+# if testa uma condição lógica. Se for True (Verdadeiro), executa os códigos que estão dentro do if.
+# else executa os códigos se a condição for False (Falso).
 if (idade > 17):
     print('Maior de idade')
 else:
     print('É menor de idade')
 
-
-verdadeiro = True # ou False
+#A variável do tipo booleana armazena True ou False
+verdadeiro = True
 print(nome,"é do Para?")
 print(verdadeiro)
 if (not verdadeiro):
@@ -53,6 +61,7 @@ v2 = True
 f1 = False
 f2 = False
 
+# a palavra "and" serve para testarmos duas condições lógicas no mesmo if e as duas condições DEVEM ser True
 if (v1 and v2):
     print('Operador AND deu verdadeiro')
 else:
@@ -63,9 +72,7 @@ if (v1 and f1):
 else:
     print('Operador AND deu falso')
 
-
-
-
+# a palavra "or" por outro lado, basta que pelo menos uma das condições lógicas sejam verdadeiras.
 if (v1 or v2):
     print('Operador OR deu verdadeiro')
 else:
@@ -76,7 +83,7 @@ if (v1 or f1):
 else:
     print('Operador OR deu falso')
 
-
+# o caractere "^" testa as duas condições lógicas e somente uma delas devem ser True
 if (v1 ^ v2):
     print('Operador XOR deu verdadeiro')
 else:
